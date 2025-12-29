43734ea41c9834ed8e209ecdda3e0e26feee5d09
PranavWeb3Dev <pranavsy.web3@gmail.com>
2025-12-29 11:16:04 +0530

    Updating the code file

--- part1/primitives.js
+++ part1/primitives.js
@@ -30,3 +30,58 @@ let str1 = ` I am showing the number ${num1}`; // String Interpolation
 
 console.log("Exapmle of the string interpolation = " + str1);
 
+// Boolean
+console.log("------------------------");
+let isLoggedIn = false;
+
+console.log(!isLoggedIn);
+console.log("------------------------");
+console.log("isLoggedIn is the type of " + typeof isLoggedIn );
+
+console.log("------------------------");
+
+// BigInt
+
+let bigValue = 37453719937n; // This is the one way to define the BigInt.
+
+let bigValue2 = BigInt(43437728930); // Sevonfway to define the BigInt without new operator.
+
+console.log(bigValue + 'and' + bigValue2);
+console.log("The Type of the Big Value One is : " + typeof bigValue + " and The Type of the Big Value Two is : " + typeof bigValue2);
+console.log("------------------------");
+
+//Symobol
+
+let symOne = Symbol("Foo");
+let symTwo = Symbol("Foo");
+
+console.log("Checcking if the symOne and symTwo is same or different using the comparison of them " + (symOne == symTwo));
+
+/*
+Instade of the using the same Symbol("Foo") for the both the symOne and symTwo we are getting the False result because when we define the Symbol then it allocate the memory with the it's own reference so instead we are using the Symbol("Foo") for both of them we got two different references with two different memory locations. So we can say that the Symbol is uses same functions like object but the Symbol is primative type and the Object is non priitive type but we can make the symbol as object by using the 'object' method. Below is the example of it.
+*/
+console.log("------------------------");
+
+const symToObj = Symbol("obj"); // Symbol Defined
+const symObj = Object(symToObj); // Converted Symbol to Object using the Object method
+
+console.log("Type of the symToObj is : " + typeof symToObj + " and Type of the symObj is : " + typeof symObj);
+
+// Undefined
+
+let name; // By default it is undefined because we have not defined
+console.log("------------------------");
+console.log(name);
+
+// Null
+
+let empty = null;  // Completly it is null not having the aloction on memory.
+
+console.log("------------------------");
+
+console.log(empty);
+
+console.log("Type of the null is : " + typeof empty); // Object
+
+
+
